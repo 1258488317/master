28b8623887cce828ecb5e4919047f4b6bad57e39
biqi <1258488317@qq.com>
2018-04-03 17:14:51 +0800

    ooo

---
+++ python/FOR.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+for i in range(1,32 ):
+    print(i)
+else:
+    print('the loop is over')
---
+++ python/break.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+while True:
+    s=(input('Enter somrthing :'))
+    if s=='quit':
+        break
+    print('length of the string is:',len(s))
+print('done')
